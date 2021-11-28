76665987d362207cff5aa93130a3eac02762b4be
cxxxr <g23tlm@gmail.com>
2021-11-28 21:30:48 +0900

    add Execute method, mini interpreter

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "log"
 import "regexp"
 import "fmt"
+import "strconv"
 
 // Token
 type Token int
@@ -13,6 +14,7 @@ const (
 	operator_begin
 	PLUS
 	MINUS
+	PRINT
 	operator_end
 )
 
@@ -22,6 +24,8 @@ func token(literal string) Token {
 		return PLUS
 	case "-":
 		return MINUS
+	case ".s":
+		return PRINT
 	default:
 		return INT
 	}
@@ -43,7 +47,7 @@ func (w Word) String() string {
 var tokenizer *regexp.Regexp
 
 func init() {
-	tokenizer = regexp.MustCompile(`\s*(\w+|[+-])`)
+	tokenizer = regexp.MustCompile(`\s*([\w.]+|[+-])`)
 }
 
 // TODO: error handling
@@ -61,13 +65,121 @@ func Parse(code string) []Word {
 	return words
 }
 
+// Execute
+
+type Cell interface {
+	String() string
+}
+
+type Int struct {
+	v int
+}
+
+func (i Int) String() string {
+	return fmt.Sprint(i.v)
+}
+
+type Stack struct {
+	data []Cell
+}
+
+type Env struct {
+	stack *Stack
+}
+
+func NewInt(v int) *Int {
+	return &Int{v: v}
+}
+
+func (stack *Stack) Peek() (Cell, error) {
+	if len(stack.data) == 0 {
+		return nil, fmt.Errorf("stack underflow: %#v", stack.data)
+	}
+	v := stack.data[len(stack.data)-1]
+	return v, nil
+}
+
+func (stack *Stack) Pop() (Cell, error) {
+	v, err := stack.Peek()
+	if err != nil {
+		return nil, err
+	}
+	stack.data = stack.data[:len(stack.data)-1]
+	log.Printf("poped: %#v", stack.data)
+	return v, nil
+}
+
+func (stack *Stack) Push(c Cell) {
+	stack.data = append(stack.data, c)
+	log.Printf("pushed: %#v", stack.data)
+}
+
+func (env *Env) Execute(words []Word) error {
+	stack := env.stack
+
+	for _, word := range words {
+		switch word.tok {
+		case PLUS:
+			// operator: + x y
+			// pop: y rhs
+			// pop: x lhs
+			rhs, err := stack.Pop()
+			if err != nil {
+				return err
+			}
+
+			lhs, err := stack.Pop()
+			if err != nil {
+				return err
+			}
+
+			y, ok := rhs.(*Int)
+			if !ok {
+				return fmt.Errorf("It's not Int: %#v", rhs)
+			}
+
+			x, ok := lhs.(*Int)
+			if !ok {
+				return fmt.Errorf("It's not Int: %#v", lhs)
+			}
+
+			stack.Push(NewInt(x.v + y.v))
+		case MINUS:
+			log.Fatal("TODO")
+		case INT:
+			v, err := strconv.ParseInt(word.lit, 10, 32) // REVIEW: bitは32でいいらしい
+			if err != nil {
+				log.Fatalf("unexpected int: %s", word.lit)
+			}
+			stack.Push(NewInt(int(v)))
+		case PRINT:
+			fmt.Print("[")
+			for i, v := range stack.data {
+				if i == 0 {
+					fmt.Printf("%v", v)
+				} else {
+					fmt.Printf(" %v", v)
+				}
+			}
+			fmt.Print("]")
+			fmt.Println()
+		}
+	}
+
+	return nil
+}
+
 func main() {
 	log.SetPrefix("forth: ")
 	log.SetFlags(0)
 
-	tokens := Parse("100 200 +")
-	log.Print(tokens)
+	words := Parse("100 200 300 400 + .s")
+	log.Print(words)
 
-	tokens = Parse("100 200 -")
-	log.Print(tokens)
+	env := Env{stack: &Stack{}}
+	if err := env.Execute(words); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(env.stack)
 }
--- main_test.go
+++ main_test.go
@@ -49,3 +49,24 @@ func TestParse(t *testing.T) {
 	testParse(t, "+",
 		[]Word{newPlusWord()})
 }
+
+func TestRegression1(t *testing.T) {
+	words := Parse("100 200 +")
+
+	env := Env{stack: &Stack{}}
+	if err := env.Execute(words); err != nil {
+		t.Fatal(err)
+	}
+
+	actual, err := env.stack.Peek()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	v := actual.(*Int)
+	if v.v != 300 {
+		t.Fatalf("wrong something: %v", v)
+	}
+
+	println(v.v)
+}
