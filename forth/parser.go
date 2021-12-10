9a708c458fa9b65cb96d69703fa390f2e2dda159
cxxxr <g23tlm@gmail.com>
2021-12-11 01:04:49 +0900

    revert `change tokenizer to parse integers` 38cf59ed

--- forth/parser.go
+++ forth/parser.go
@@ -3,16 +3,10 @@ package forth
 import (
 	"fmt"
 	"regexp"
-	"strconv"
 )
-
- // TODO: main.goの定義との重複を解消する
-const intBitSize = 32
-type ForthInt int32
 
 type Token struct {
 	Lit string // TODO: unexport
-	intValue ForthInt // TODO: Litがintの場合だけ値が入るがこれは良くないので直す
 }
 
 func (t Token) String() string {
@@ -29,19 +23,11 @@ func init() {
 	tokenizer = regexp.MustCompile(`\s*(\S+)`)
 }
 
-func parseToken(lit string) Token {
-	if v, err := strconv.ParseInt(lit, 10, intBitSize); err == nil {
-		return Token{Lit: lit, intValue: ForthInt(v)}
-	} else {
-		return Token{Lit: lit}
-	}
-}
-
 func Parse(code string) []Token {
 	tokens := make([]Token, 0)
 	for _, group := range tokenizer.FindAllStringSubmatch(code, -1) {
 		lit := group[1]
-		tokens = append(tokens, parseToken(lit))
+		tokens = append(tokens, Token{lit})
 	}
 	return tokens
 }
